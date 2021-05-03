c3bf7643c5da49baa48dc7d8271e9ca039e739e7
kouki9115 <kouki.n9115@gmail.com>
2021-05-03 07:49:44 +0000

    Update User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,29 @@
 class UsersController < ApplicationController
   def index
+    @users=User.all
   end
 
   def show
+   @user=User.find(params[:id])
   end
 
   def edit
-  end
-  
-  def update
-  end
-  
-  def destroy
+   @user=User.find(params[:id])
   end
 
+  def update
+   @user=User.find(params[:id])
+   @user.update(params_user)
+   redirect_to user_path(@user)
+  end
+
+  def destroy
+   @user=User.find(params[:id])
+   #後程対応予定
+  end
+
+  private
+  def params_user
+  params.require(:user).permit(:profile_image,:name,:introduction)
+  end
 end
