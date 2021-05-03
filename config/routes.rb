0eae305908281b4d1e1e934a2799c1e6b8038ec1
kouki9115 <kouki.n9115@gmail.com>
2021-05-03 04:51:32 +0000

    add controller view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+  get 'genres/edit'
+  get 'blogs/new'
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   
   root to:'homes#top'
