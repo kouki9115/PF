b59b3e83d2c45eb49999401a3766e9669e0d314b
kouki9115 <kouki.n9115@gmail.com>
2021-05-03 07:04:50 +0000

    Update blogs

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '31dc694f1c402492785966710587adcdeb4d161ecc623d799944a098940ba0a7788cf895a2f226d4b545ec6cfe3e30fe371682d4cc181f8aca740e01d680d9fd'
