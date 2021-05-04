9d3af52119f4098ac48bdf4fa243ad2110de2ab3
kouki9115 <kouki.n9115@gmail.com>
2021-05-04 07:07:43 +0000

    Update Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :blogs, dependent: :destroy
-
+  has_many :blog_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
