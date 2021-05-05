824414906a883c8a4f875bd9efe5bf0ae00aa035
kouki9115 <kouki.n9115@gmail.com>
2021-05-05 19:32:02 +0000

    Finish blog bootstrap

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
     blog = Blog.find(params[:blog_id])
     favorite = current_user.favorites.new(blog_id: blog.id)
     favorite.save
-    redirect_to blogs_path
+    redirect_to request.referer
   end
 
   def destroy
     blog = Blog.find(params[:blog_id])
     favorite = current_user.favorites.find_by(blog_id: blog.id)
     favorite.destroy
-    redirect_to blogs_path
+    redirect_to request.referer
   end
 
 end
