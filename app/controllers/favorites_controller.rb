77c70c256258ac033112bfeccc993033f9082184
kouki9115 <kouki.n9115@gmail.com>
2021-05-12 12:47:01 +0000

    add Asynchronous communication

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    blog = Blog.find(params[:blog_id])
-    favorite = current_user.favorites.new(blog_id: blog.id)
+    @blog = Blog.find(params[:blog_id])
+    favorite = current_user.favorites.new(blog_id: @blog.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
-    blog = Blog.find(params[:blog_id])
-    favorite = current_user.favorites.find_by(blog_id: blog.id)
+    @blog = Blog.find(params[:blog_id])
+    favorite = current_user.favorites.find_by(blog_id: @blog.id)
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
 end
