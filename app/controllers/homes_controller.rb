8d8d9d6000b492b6e113774328e602c967582012
kouki9115 <kouki.n9115@gmail.com>
2021-05-13 08:38:09 +0000

    Bootstrap border Awe,gest Limit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,11 @@
 class HomesController < ApplicationController
   def top
-    @blogs=Blog.page(params[:page]).reverse_order
+    @blogs_new = Blog.all.order(created_at: "DESC").limit(10)
+    @blogs_ran = Blog.select('blogs.*', 'count(favorites.id) AS favorites')
+       .left_joins(:favorites)
+       .group('blogs.id')
+       .order('favorites DESC')
+       .limit(5)
   end
 
   def about
