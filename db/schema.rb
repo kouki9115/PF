0e80d4109dc07f597fc9430da59c4ac430ed9eaf
kouki9115 <kouki.n9115@gmail.com>
2021-05-21 10:41:10 +0000

    add mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_060156) do
+ActiveRecord::Schema.define(version: 2021_05_21_091833) do
 
   create_table "blog_comments", force: :cascade do |t|
     t.integer "blog_id"
@@ -31,6 +31,22 @@ ActiveRecord::Schema.define(version: 2021_05_15_060156) do
     t.string "price"
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
